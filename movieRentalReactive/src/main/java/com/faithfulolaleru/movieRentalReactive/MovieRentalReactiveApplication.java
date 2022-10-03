441fe61295d981ee1897d735e038add19a6c8ec3
Faithful <olalerufaithful@gmail.com>
2022-10-03 12:21:24 +0100

    initial setup

---
+++ movieRentalReactive/src/main/java/com/faithfulolaleru/movieRentalReactive/MovieRentalReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.faithfulolaleru.movieRentalReactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalReactiveApplication.class, args);
+	}
+
+}
---
+++ movieRentalReactive/src/test/java/com/faithfulolaleru/movieRentalReactive/MovieRentalReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faithfulolaleru.movieRentalReactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
